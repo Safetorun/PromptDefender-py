23c88eb4bb97042839c7f6f4bbfa7a7813de2ac2
dllewellyn <danieln.llewellyn@gmail.com>
2024-03-10 19:59:04 +0000

    Adding deploy script to GH pipelines

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,5 @@ latest_tag=${latest_tag#v}
 sed -i '' -e "s/version = \".*\"/version = \"$latest_tag\"/g" wall/pyproject.toml
 
 python3 -m pip install --upgrade twine
-cd wall && python3 -m twine upload --repository testpypi dist/*  --non-interactive --username __token__ --password $PYPI_PASSWORD_TEST
+cd wall && python3 -m twine upload --repository testpypi dist/*  --non-interactive --username __token__ --password $PYPI_PASSWORD_TEST && cd ..
 cd drawbridge && python3 -m twine upload --repository testpypi dist/*  --non-interactive --username __token__ --password $PYPI_PASSWORD_TEST
