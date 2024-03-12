f36c523d7d7d4fce2062a9ad44ced972ee21ca2f
dllewellyn <danieln.llewellyn@gmail.com>
2024-03-12 21:21:09 +0000

    Adding set version script

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -4,7 +4,7 @@ python3 -m pip install --upgrade twine build
 
 # Build
 cd wall && python3 -m build && cd ..
-cd drawbridge && python3 -m build
+cd drawbridge && python3 -m build && cd ..
 
 # Deploy
 cd wall && python3 -m twine upload --repository dist/*  --non-interactive --username __token__ --password $PYPI_PASSWORD_TEST && cd ..
