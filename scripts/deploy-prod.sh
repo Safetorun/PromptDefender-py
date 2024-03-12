64486583a4080c21ed50d266decfc2badcc7c97a
dllewellyn <danieln.llewellyn@gmail.com>
2024-03-12 21:04:59 +0000

    Adding deploy script to GH pipelines

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,10 +1,15 @@
 # Get the latest tag
 latest_tag=$(git describe --tags)
 
+echo $latest_tag
+
 latest_tag=${latest_tag#v}
 
-sed -i '' -e "s/version = \".*\"/version = \"$latest_tag\"/g" wall/pyproject.toml
+echo $latest_tag
+
+sed -i '' -e "s/version = \".*\"/version = \"$latest_tag\"/g" wall/pyproject.toml || exit 1
+sed -i '' -e "s/version = \".*\"/version = \"$latest_tag\"/g" drawbridge/pyproject.toml || exit 1
 
 python3 -m pip install --upgrade twine
-cd wall && python3 -m twine upload --repository testpypi dist/*  --non-interactive --username __token__ --password $PYPI_PASSWORD_TEST && cd ..
-cd drawbridge && python3 -m twine upload --repository testpypi dist/*  --non-interactive --username __token__ --password $PYPI_PASSWORD_TEST
+cd wall && python3 -m twine upload --repository dist/*  --non-interactive --username __token__ --password $PYPI_PASSWORD_TEST && cd ..
+cd drawbridge && python3 -m twine upload --repository dist/*  --non-interactive --username __token__ --password $PYPI_PASSWORD_TEST
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,10 @@ latest_tag=$(git describe --tags)
 
 latest_tag=${latest_tag#v}
 
-sed -i '' -e "s/version = \".*\"/version = \"$latest_tag\"/g" wall/pyproject.toml
+echo $latest_tag
+
+sed -i '' -e "s/version = \".*\"/version = \"$latest_tag\"/g" wall/pyproject.toml || exit 1
+sed -i '' -e "s/version = \".*\"/version = \"$latest_tag\"/g" drawbridge/pyproject.toml || exit 1
 
 python3 -m pip install --upgrade twine
 cd wall && python3 -m twine upload --repository testpypi dist/*  --non-interactive --username __token__ --password $PYPI_PASSWORD_TEST && cd ..
