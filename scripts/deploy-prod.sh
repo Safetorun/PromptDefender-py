16240e3b943b21acc3346529da6844301c7f1a40
dllewellyn <danieln.llewellyn@gmail.com>
2024-03-12 21:19:43 +0000

    Adding set version script

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,5 +1,11 @@
 # Get the latest tag
 python scripts/set_version.py
-python3 -m pip install --upgrade twine
+python3 -m pip install --upgrade twine build
+
+# Build
+cd wall && python3 -m build && cd ..
+cd drawbridge && python3 -m build
+
+# Deploy
 cd wall && python3 -m twine upload --repository dist/*  --non-interactive --username __token__ --password $PYPI_PASSWORD_TEST && cd ..
 cd drawbridge && python3 -m twine upload --repository dist/*  --non-interactive --username __token__ --password $PYPI_PASSWORD_TEST
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,10 @@
 python scripts/set_version.py
-python3 -m pip install --upgrade twine
+python3 -m pip install --upgrade twine build
+
+# Build
+cd wall && python3 -m build && cd ..
+cd drawbridge && python3 -m build
+
+# Deploy
 cd wall && python3 -m twine upload --repository testpypi dist/*  --non-interactive --username __token__ --password $PYPI_PASSWORD_TEST && cd ..
 cd drawbridge && python3 -m twine upload --repository testpypi dist/*  --non-interactive --username __token__ --password $PYPI_PASSWORD_TEST
