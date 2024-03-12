2584a7d35faa88a7a337560cae9b4023ec4870fa
dllewellyn <danieln.llewellyn@gmail.com>
2024-03-12 21:11:33 +0000

    Adding deploy script to GH pipelines

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -7,8 +7,8 @@ latest_tag=${latest_tag#v}
 
 echo $latest_tag
 
-sed -i '' -e "s/version = \".*\"/version = \"$latest_tag\"/g" wall/pyproject.toml || exit 1
-sed -i '' -e "s/version = \".*\"/version = \"$latest_tag\"/g" drawbridge/pyproject.toml || exit 1
+sed -i -e "s/version = \".*\"/version = \"$latest_tag\"/g" wall/pyproject.toml || exit 1 || exit 1
+sed -i -e "s/version = \".*\"/version = \"$latest_tag\"/g" drawbridge/pyproject.toml || exit 1
 
 python3 -m pip install --upgrade twine
 cd wall && python3 -m twine upload --repository dist/*  --non-interactive --username __token__ --password $PYPI_PASSWORD_TEST && cd ..
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,8 +7,8 @@ echo $latest_tag
 echo `pwd`
 echo `ls wall`
 
-sed -i '' -e "s/version = \".*\"/version = \"$latest_tag\"/g" wall/pyproject.toml || exit 1
-sed -i '' -e "s/version = \".*\"/version = \"$latest_tag\"/g" drawbridge/pyproject.toml || exit 1
+sed -i -e "s/version = \".*\"/version = \"$latest_tag\"/g" wall/pyproject.toml || exit 1 || exit 1
+sed -i -e "s/version = \".*\"/version = \"$latest_tag\"/g" drawbridge/pyproject.toml || exit 1
 
 python3 -m pip install --upgrade twine
 cd wall && python3 -m twine upload --repository testpypi dist/*  --non-interactive --username __token__ --password $PYPI_PASSWORD_TEST && cd ..
